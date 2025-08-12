5cd996e4f602bee01ed28ce7d4a0e64a82b738eb
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-08-12 14:53:33 +0200

    first commit

---
+++ src/main/kotlin/techcourse/herobeans/HeroBeansApplication.kt
@@ -0,0 +1,11 @@
+package techcourse.herobeans
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HeroBeansApplication
+
+fun main(args: Array<String>) {
+    runApplication<HeroBeansApplication>(*args)
+}
---
+++ src/test/kotlin/techcourse/herobeans/HeroBeansApplicationTests.kt
@@ -0,0 +1,13 @@
+package techcourse.herobeans
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HeroBeansApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
