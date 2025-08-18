5d63f71cc12181a7a445fb387ad056526587ed1c
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-08-18 17:21:23 +0200

    feat(WebMvcConfigurer): add new path

--- src/main/kotlin/techcourse/herobeans/configuration/WebMvcConfigurer.kt
+++ src/main/kotlin/techcourse/herobeans/configuration/WebMvcConfigurer.kt
@@ -14,6 +14,7 @@ class WebMvcConfigurer(
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(jwtAuthInterceptor)
             .addPathPatterns(
+                "/api/member/**",
                 "/api/user/wishes/**",
                 "/api/admin/**",
                 "api/admin/stats/**",
