fe3febddeb0c8c7a5be315cc46e36e41dbfe2e2e
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-08-19 19:42:04 +0200

    chor: move coffe-spec and spec-builder in one directory

--- src/main/kotlin/techcourse/herobeans/controller/GuestController.kt
+++ src/main/kotlin/techcourse/herobeans/controller/GuestController.kt
@@ -1,8 +1,8 @@
 package techcourse.herobeans.controller
 
-import org.springframework.data.domain.Page
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.data.domain.Page
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
