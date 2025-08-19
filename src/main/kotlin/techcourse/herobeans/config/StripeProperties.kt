fd7747e5e01aec031a2a451d7224cdebb5def6ea
Mina Kim <mina.kim@deliveryhero.com>
2025-08-19 10:28:01 +0200

    fix(stripe-properties): add default values for configuration properties

--- src/main/kotlin/techcourse/herobeans/config/StripeProperties.kt
+++ src/main/kotlin/techcourse/herobeans/config/StripeProperties.kt
@@ -2,8 +2,9 @@ package techcourse.herobeans.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
+// TODO: delete this default value
 @ConfigurationProperties("stripe")
 class StripeProperties(
-    val secretKey: String,
-    val createPaymentIntentUrl: String,
+    val secretKey: String = "sk_test_default",
+    val createPaymentIntentUrl: String = "https://api.stripe.com",
 )
