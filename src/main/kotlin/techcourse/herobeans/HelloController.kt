9172f7104e0e3d265bb84a962813fa7f2f432898
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-08-19 18:50:00 +0200

    feat(hello world): implement first simple endpoint hello world

---
+++ src/main/kotlin/techcourse/herobeans/HelloController.kt
@@ -0,0 +1,12 @@
+package techcourse.herobeans
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping
+    fun hello(): String {
+        return "Hello World.. this is Hero Beans"
+    }
+}
