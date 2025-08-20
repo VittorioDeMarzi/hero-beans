b54d2b33c4c017d8f52f14ab724b80b814c266c1
pebencze <petra.bencze@deliveryhero.com>
2025-08-20 17:47:48 +0200

    refactor: AuthenticationService.kt publishes event, not AuthenticationController.kt

--- src/main/kotlin/techcourse/herobeans/controller/AuthenticationController.kt
+++ src/main/kotlin/techcourse/herobeans/controller/AuthenticationController.kt
@@ -2,7 +2,6 @@ package techcourse.herobeans.controller
 
 import ecommerce.annotation.LoginMember
 import jakarta.validation.Valid
-import org.springframework.context.ApplicationEventPublisher
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,21 +13,16 @@ import techcourse.herobeans.dto.LoginRequest
 import techcourse.herobeans.dto.MemberDto
 import techcourse.herobeans.dto.RegistrationRequest
 import techcourse.herobeans.dto.TokenResponse
-import techcourse.herobeans.event.UserRegisteredEvent
 import techcourse.herobeans.service.AuthenticationService
 
 @RestController
 @RequestMapping("/api/members")
-class AuthenticationController(
-    private val authenticationService: AuthenticationService,
-    private val applicationEventPublisher: ApplicationEventPublisher,
-) {
+class AuthenticationController(private val authenticationService: AuthenticationService) {
     @PostMapping("/register")
     fun registerMember(
         @Valid @RequestBody request: RegistrationRequest,
     ): ResponseEntity<TokenResponse> {
         val token = authenticationService.register(request)
-        applicationEventPublisher.publishEvent(UserRegisteredEvent(request.email))
         return ResponseEntity.status(HttpStatus.CREATED).body(token)
     }
 
