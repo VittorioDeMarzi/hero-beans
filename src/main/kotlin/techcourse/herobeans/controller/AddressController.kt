0f1bb3c7c2ffbf694bed5d10e25ed85b34185898
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-08-20 15:43:22 +0200

    feat(controller): add log info

--- src/main/kotlin/techcourse/herobeans/controller/AddressController.kt
+++ src/main/kotlin/techcourse/herobeans/controller/AddressController.kt
@@ -3,6 +3,7 @@ package techcourse.herobeans.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
+import mu.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -20,6 +21,8 @@ import techcourse.herobeans.dto.MemberDto
 import techcourse.herobeans.dto.UpdateAddressRequest
 import techcourse.herobeans.service.AddressService
 
+private val log = KotlinLogging.logger {}
+
 @Tag(name = "Address", description = "Manage member addresses")
 @SecurityRequirement(name = "bearerAuth")
 @RestController
@@ -33,6 +36,7 @@ class AddressController(
         @LoginMember member: MemberDto,
         @RequestBody request: AddressRequest,
     ): ResponseEntity<AddressDto> {
+        log.info { "api.address.create requested memberId=${member.id}" }
         val newAddress = addressService.createAddress(request, member.id)
         return ResponseEntity.status(HttpStatus.CREATED).body(newAddress)
     }
@@ -43,6 +47,7 @@ class AddressController(
         @LoginMember member: MemberDto,
         @PathVariable addressId: Long,
     ): ResponseEntity<Void> {
+        log.info { "api.address.delete requested memberId=${member.id} addressId=$addressId" }
         addressService.removeAddress(addressId, member.id)
         return ResponseEntity.noContent().build()
     }
@@ -54,6 +59,7 @@ class AddressController(
         @RequestBody request: UpdateAddressRequest,
         @PathVariable addressId: Long,
     ): ResponseEntity<AddressDto> {
+        log.info { "api.address.update requested memberId=${member.id} addressId=$addressId" }
         val updatedAddress = addressService.updateAddress(addressId, member.id, request)
         return ResponseEntity.ok(updatedAddress)
     }
@@ -63,6 +69,7 @@ class AddressController(
     fun getAllAddresses(
         @LoginMember member: MemberDto,
     ): ResponseEntity<List<AddressDto>> {
+        log.debug { "api.address.list requested memberId=${member.id}" }
         val addresses = addressService.getAllAddresses(member.id)
         return ResponseEntity.ok(addresses)
     }
