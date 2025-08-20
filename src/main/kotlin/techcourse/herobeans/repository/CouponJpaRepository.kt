4b124509b3fce2ec69a4cb6b4fed0494c521ca46
pebencze <petra.bencze@deliveryhero.com>
2025-08-20 15:58:53 +0200

    feat(event): add UserRegisteredEvent.kt, publish event inside AuthenticationController.kt on registration, listen for event in WelcomeCouponListener.kt and issue Coupon

--- src/main/kotlin/techcourse/herobeans/repository/CouponJpaRepository.kt
+++ src/main/kotlin/techcourse/herobeans/repository/CouponJpaRepository.kt
@@ -5,4 +5,6 @@ import techcourse.herobeans.entity.Coupon
 
 interface CouponJpaRepository : JpaRepository<Coupon, Long> {
     fun findByCodeAndActiveTrue(code: String): Coupon?
+
+    fun findByUserMail(userMail: String): List<Coupon>
 }
