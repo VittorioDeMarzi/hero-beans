f85abb4bdba1d0bf6d683bbbc22721af387db03e
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-08-20 11:38:54 +0200

    chore(log): add dependency for logging
    - add kotlin-logging dependency for idiomatic Kotlin logger API
    - set root logging level to WARN to reduce noise from Spring/Hibernate
    - set techcourse.herobeans package logging level to INFO to capture business events, warnings and errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.mindrot:jbcrypt:0.4")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
