e4d337eedcf261b2516c0c85952cb3c8555fc12e
Mina Kim <mina.kim@deliveryhero.com>
2025-08-21 01:22:13 +0200

    refactor(exception): set StripeProcessingException as sealed class from open class
    - Consistency with other exception groups

--- src/main/kotlin/techcourse/herobeans/exception/StripeProcessingException.kt
+++ src/main/kotlin/techcourse/herobeans/exception/StripeProcessingException.kt
@@ -1,6 +1,6 @@
 package techcourse.herobeans.exception
 
-open class StripeProcessingException(message: String, override val cause: Throwable?) : RuntimeException(message, cause)
+sealed class StripeProcessingException(message: String, override val cause: Throwable?) : RuntimeException(message, cause)
 
 class StripeClientException(message: String, override val cause: Throwable?) : StripeProcessingException(message, cause)
 
