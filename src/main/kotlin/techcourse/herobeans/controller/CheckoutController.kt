ff4cd0f1d2287d5bcd114148be3f3f8ad6ff19d2
Mina Kim <mina.kim@deliveryhero.com>
2025-08-22 10:57:01 +0200

    chore: merge dev to this branch, cleanup, add new properties

--- src/main/kotlin/techcourse/herobeans/controller/CheckoutController.kt
+++ src/main/kotlin/techcourse/herobeans/controller/CheckoutController.kt
@@ -1,6 +1,5 @@
 package techcourse.herobeans.controller
 
-import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
