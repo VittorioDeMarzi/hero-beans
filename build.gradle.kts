8e1612e677d5a2c46189c8cc4f2408d1dd409065
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-08-24 15:55:47 +0200

    chore(dependencies): remove double injection for spring-boot-starter-web

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
